3d896d14e6da57446dbf289d683daf436ee2148c
Neil_Hsieh <neil605164@gmail.com>
2017-09-11 23:57:10 +0800

    addstore頁面製作中

--- resources/views/store/addstore.blade.php
+++ resources/views/store/addstore.blade.php
@@ -5,7 +5,10 @@
         {{ csrf_field() }}
         <div class="basic-info">
             <span class="basic-title">基本資料 :</span>
-
+            <span class="basic-date">名稱 ：<input type="text" class="input-name" placeholder="請輸入店家名稱"></span>
+            <span class="basic-date">簡介 ：<input type="text" class="input-intr" placeholder="請輸入店家簡介"></span>
+            <span class="basic-date">電話 ：<input type="text" class="input-tel" placeholder="請輸入店家電話"></span>
+            <span class="basic-date">地址 ：<input type="text" class="input-addr" placeholder="請輸入店家地址"></span>
         </div>
     </form>
 @endsection
