57a92669ba428012bff4a23a3fb2d38e286553cc
Neil_Hsieh <neil605164@gmail.com>
2017-09-11 20:59:36 +0800

    first commit

---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel 訂便當系統</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <div class="wrap">
+    	<div class="header">
+    		<div class="header-nav">
+    			<ul class="header-menu">
+    				<li><a href="#" class="header-home-icon" title="首頁"></a></li>
+					<!-- <li><a href="#" class="header-meal-icon" title="訂餐"></a></li> -->
+    				<li><a href="#" class="header-add-icon" title="新增店家"></a></li>
+					<li><a href="#" class="header-edit-icon" title="管理店家"></a></li>
+					<li><a href="#" class="header-search-icon" title="搜尋"></a></li>
+    			</ul>
+    		</div>
+    	</div>
+		<div class="content"></div>
+		<div class="footer"></div>
+    </div>
+</body>
+</html>
